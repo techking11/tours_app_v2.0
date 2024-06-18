a9be5006671d631713207aa4b63c3569006d5321
techking11 <techking1132000@gmail.com>
2024-06-18 17:43:09 +0700

    average rating on review and tour

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
+const { info } = require('console');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
     review: {
       type: String,
-      required: [true, 'Review cannot be empty !'],
+      required: [true, 'Review should not be empty !'],
     },
     rating: {
       type: Number,
@@ -23,7 +25,7 @@ const reviewSchema = new mongoose.Schema(
     tour: {
       type: mongoose.Schema.ObjectId,
       ref: 'Tour',
-      required: [true, 'Review must belong to a user.'],
+      required: [true, 'Review must belong to a tour.'],
     },
   },
   {
@@ -38,6 +40,30 @@ reviewSchema.pre(/^find/, function (next) {
     select: 'name photo',
   });
   next();
+});
+
+reviewSchema.statics.calcAverageRating = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        numRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  info(stats);
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsQuantity: stats[0].numRating,
+    ratingsAverage: stats[0].avgRating,
+  });
+};
+
+reviewSchema.post('save', function () {
+  this.constructor.calcAverageRating(this.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
